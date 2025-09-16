a0c704c1e8e3fddc471113ae644dd447e001beeb
Tiago Cardoso <tiago.mncardoso@gmail.com>
2025-09-16 05:47:26 -0300

    chore: 🔧 fix application name for logs

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,13 +3,13 @@ package logger
 import "log/slog"
 
 func Info(msg string, args ...any) {
-	slog.Info("RateLimiter middleware> "+msg, args...)
+	slog.Info("PDFApiGenerator middleware> "+msg, args...)
 }
 
 func Warn(msg string, args ...any) {
-	slog.Warn("RateLimiter middleware> "+msg, args...)
+	slog.Warn("PDFApiGenerator middleware> "+msg, args...)
 }
 
 func Error(msg string, args ...any) {
-	slog.Error("RateLimiter middleware> "+msg, args...)
+	slog.Error("PDFApiGenerator middleware> "+msg, args...)
 }
